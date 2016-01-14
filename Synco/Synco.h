1380a3c9cb5207aa6b1caa6869c2ed03883708d5
yukiasai <yukiasai@gmail.com>
2016-01-14 14:23:02 +0900

    Initial Commit

---
+++ Synco/Synco.h
@@ -0,0 +1,19 @@
+//
+//  Synco.h
+//  Synco
+//
+//  Created by asai.yuki on 2016/01/14.
+//  Copyright © 2016年 yukiasai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Synco.
+FOUNDATION_EXPORT double SyncoVersionNumber;
+
+//! Project version string for Synco.
+FOUNDATION_EXPORT const unsigned char SyncoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Synco/PublicHeader.h>
+
+
