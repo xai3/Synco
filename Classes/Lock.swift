060c6da83df09384c76c108b8b35cc32aacaab35
yukiasai <yukiasai@gmail.com>
2016-01-14 15:05:06 +0900

    Add public access control.

--- Classes/Lock.swift
+++ Classes/Lock.swift
@@ -9,23 +9,23 @@
 import Foundation
 import ObjectiveC
 
-class Lock {
-    func sync<T>(@noescape closure: Void -> T) -> T {
+public class Lock {
+    public func sync<T>(@noescape closure: Void -> T) -> T {
         return Lock.sync(self, closure: closure)
     }
     
-    class func sync<T>(object: AnyObject, @noescape closure: Void -> T) -> T {
+    public class func sync<T>(object: AnyObject, @noescape closure: Void -> T) -> T {
         enter(object)
         let value = closure()
         exit(object)
         return value
     }
     
-    class func enter(object: AnyObject) {
+    public class func enter(object: AnyObject) {
         objc_sync_enter(object)
     }
     
-    class func exit(object: AnyObject) {
+    public class func exit(object: AnyObject) {
         objc_sync_exit(object)
     }
 }
